df146d474c4a586d79a6b6ec34fd56ff07e24c80
khsFirefox <rlagnltn135@smtown.com>
2023-07-14 17:05:01 +0900

    fix dependencies

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 // 개발 환경에서의 설정을 사용합니다.
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/config.json")[env];
+const config = require("./config.json")[env];
 
 // Sequelize 인스턴스를 생성합니다.
 const sequelize = new Sequelize(
